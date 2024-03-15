c3a9e94bea0e849a31d721c5a4c6e0536ea3d6f5
Mateusz Barc <mateuszbarc1996@gmail.com>
2024-03-15 16:06:19 +0100

    add gallery type - lightbox and styles css

--- pages/galeria.js
+++ pages/galeria.js
@@ -1,24 +1,58 @@
 import Header from '../components/Header';
 import styles from "../styles/galeria.module.css";
+import React, { useState } from 'react';
+import Lightbox from 'react-image-lightbox';
+import 'react-image-lightbox/style.css';
 
 
 const Galeria = () => {
+
+    const [isOpen, setIsOpen] = useState(false);
+    const [photoIndex, setPhotoIndex] = useState(0);
+
+    const images = [
+        '/galeria1.jpeg',
+        '/galeria2.jpeg',
+        '/galeria3.JPG',
+        '/galeria4.JPG',
+        '/galeria5.JPG',
+        '/galeria6.JPG',
+    ];
+
+    const openLightbox = (index) => {
+        setPhotoIndex(index);
+        setIsOpen(true);
+    };
+
+
     return (
         <div>
             <Header />
             <main>
-                <h1>Galeria</h1>
-                <div className="gallery">
-                    <img src="/galeria1.jpeg" alt="Obraz 1" style={{ width: '250px', height: '250px', objectFit: 'cover', margin: '10px' }} />
-                    <img src="/galeria2.jpeg" alt="Obraz 2" style={{ width: '250px', height: '250px', objectFit: 'cover', margin: '10px' }} />
-                    <img src="/galeria3.JPG" alt="Obraz 3" style={{ width: '250px', height: '250px', objectFit: 'cover', margin: '10px' }} />
-                    <img src="/galeria4.JPG" alt="Obraz 4" style={{ width: '250px', height: '250px', objectFit: 'cover', margin: '10px' }} />
-                    <img src="/galeria5.JPG" alt="Obraz 5" style={{ width: '250px', height: '250px', objectFit: 'cover', margin: '10px' }} />
-                    <img src="/galeria6.JPG" alt="Obraz 6" style={{ width: '250px', height: '250px', objectFit: 'cover', margin: '10px' }} />
-                    {/* Dodaj więcej zdjęć galerii */}
+                <h1 className={styles.naglowek}>Galeria</h1>
+                <div className={styles.gallery}>
+                    {images.map((image, index) => (
+                        <img
+                            key={index}
+                            src={image}
+                            alt={`Obraz ${index + 1}`}
+                            className={styles.galleryImage}
+                            onClick={() => openLightbox(index)}
+                        />
+                    ))}
                 </div>
+                {isOpen && (
+                    <Lightbox
+                        mainSrc={images[photoIndex]}
+                        nextSrc={images[(photoIndex + 1) % images.length]}
+                        prevSrc={images[(photoIndex + images.length - 1) % images.length]}
+                        onCloseRequest={() => setIsOpen(false)}
+                        onMovePrevRequest={() => setPhotoIndex((photoIndex + images.length - 1) % images.length)}
+                        onMoveNextRequest={() => setPhotoIndex((photoIndex + 1) % images.length)}
+                    />
+                )}
             </main>
-        </div>
+        </div >
     );
 };
 

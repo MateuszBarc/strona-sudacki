901a4c81a64c8b8562b3ead6f438a302b72c45bb
Mateusz Barc <mateuszbarc1996@gmail.com>
2024-03-16 13:34:32 +0100

    Changing a lightbox to a component

---
+++ components/LightBox.js
@@ -0,0 +1,55 @@
+import styles from "../styles/galeria.module.css";
+import React, { useState } from 'react';
+import Lightbox from 'react-image-lightbox';
+import 'react-image-lightbox/style.css';
+
+const LightBox = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    const [photoIndex, setPhotoIndex] = useState(0);
+
+    const images = [
+        '/galeria1.jpeg',
+        '/galeria2.jpeg',
+        '/galeria3.JPG',
+        '/galeria4.JPG',
+        '/galeria5.JPG',
+        '/galeria6.JPG',
+    ];
+
+    const openLightbox = (index) => {
+        setPhotoIndex(index);
+        setIsOpen(true);
+    };
+
+    return (
+        <div>
+
+            <main>
+                <h1 className={styles.naglowek}>Galeria</h1>
+                <div className={styles.gallery}>
+                    {images.map((image, index) => (
+                        <img
+                            key={index}
+                            src={image}
+                            alt={`Obraz ${index + 1}`}
+                            className={styles.galleryImage}
+                            onClick={() => openLightbox(index)}
+                        />
+                    ))}
+                </div>
+                {isOpen && (
+                    <Lightbox
+                        mainSrc={images[photoIndex]}
+                        nextSrc={images[(photoIndex + 1) % images.length]}
+                        prevSrc={images[(photoIndex + images.length - 1) % images.length]}
+                        onCloseRequest={() => setIsOpen(false)}
+                        onMovePrevRequest={() => setPhotoIndex((photoIndex + images.length - 1) % images.length)}
+                        onMoveNextRequest={() => setPhotoIndex((photoIndex + 1) % images.length)}
+                    />
+                )}
+            </main>
+        </div >
+    );
+}
+
+export default LightBox;
--- pages/galeria.js
+++ pages/galeria.js
@@ -3,7 +3,7 @@ import styles from "../styles/galeria.module.css";
 import React, { useState } from 'react';
 import Lightbox from 'react-image-lightbox';
 import 'react-image-lightbox/style.css';
-
+import LightBox from '@/components/LightBox';
 
 const Galeria = () => {
 
@@ -29,28 +29,7 @@ const Galeria = () => {
         <div>
             <Header />
             <main>
-                <h1 className={styles.naglowek}>Galeria</h1>
-                <div className={styles.gallery}>
-                    {images.map((image, index) => (
-                        <img
-                            key={index}
-                            src={image}
-                            alt={`Obraz ${index + 1}`}
-                            className={styles.galleryImage}
-                            onClick={() => openLightbox(index)}
-                        />
-                    ))}
-                </div>
-                {isOpen && (
-                    <Lightbox
-                        mainSrc={images[photoIndex]}
-                        nextSrc={images[(photoIndex + 1) % images.length]}
-                        prevSrc={images[(photoIndex + images.length - 1) % images.length]}
-                        onCloseRequest={() => setIsOpen(false)}
-                        onMovePrevRequest={() => setPhotoIndex((photoIndex + images.length - 1) % images.length)}
-                        onMoveNextRequest={() => setPhotoIndex((photoIndex + 1) % images.length)}
-                    />
-                )}
+                <LightBox />
             </main>
         </div >
     );

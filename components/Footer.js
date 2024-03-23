354e3402f9794dcd85c0df9d1d9d709bf00c3454
Mateusz Barc <mateuszbarc1996@gmail.com>
2024-03-23 13:44:17 +0100

    adding Facebook and Instagram logos and social media links to the footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,5 +1,7 @@
 import OpenStreetMap from './OpenStreetMap';
 import Link from 'next/link';
+import Image from 'next/image';
+
 const Footer = () => {
     return (
         <footer style={{ backgroundColor: '#f0f0f0', padding: '20px', display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
@@ -8,14 +10,23 @@ const Footer = () => {
                 <p>Adres: Lubenia 618, 36-042 Lubenia</p>
                 <p>Godziny otwarcia: 8-21</p>
                 <Link href="/" >
-                    <img src="/LogoCzarne.png" alt="Logo" style={{ width: '250px', height: '250px', borderRadius: '250px' }} />
+                    <Image src="/LogoCzarne.png" alt="Logo" width={250} height={250} style={{ borderRadius: '250px' }} />
                 </Link>
             </div>
-            <div>
-                <Link href="https://www.google.com/search?q=maraton+lubenia&oq=marato&gs_lcrp=EgZjaHJvbWUqDwgAECMYJxjjAhiABBiKBTIPCAAQIxgnGOMCGIAEGIoFMhUIARAuGCcYrwEYxwEYgAQYigUYjgUyBggCEEUYOTIQCAMQABiDARixAxjJAxiABDINCAQQABiDARixAxiABDINCAUQABiSAxiABBiKBTIGCAYQRRg8MgYIBxBFGD3SAQc5MTJqMGo3qAIAsAIA&sourceid=chrome&ie=UTF-8"> <OpenStreetMap center={{ lat: 49.9329252, lng: 21.9091142 }} zoom={25} /></Link>
-                {/* Tutaj dodajemy zoom na mapie oraz wpolrzedne do mapy  */}
-
+            <div style={{ display: 'flex', flexDirection: 'column' }}>
+                <div style={{ marginBottom: '10px' }}>
+                    <Link href="https://www.facebook.com/profile.php?id=61554392384818">
+                        <Image src="/facebook-logo.png" alt="Facebook" width={40} height={40} style={{ marginRight: '10px' }} />
+                    </Link>
+                </div>
+                <div style={{ marginTop: '10px' }}>
+                    <Link href="https://www.instagram.com/michal.sudacki/">
+                        <Image src="/instagram-logo.png" alt="Instagram" width={40} height={40} style={{ marginRight: '10px' }} />
+                    </Link>
+                </div>
+                <OpenStreetMap center={{ lat: 49.9329252, lng: 21.9091142 }} zoom={25} />
             </div>
+
         </footer>
     );
 };
